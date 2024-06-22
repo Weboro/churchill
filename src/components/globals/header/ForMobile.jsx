8b92332bb15526d57acfc7c8cc69c782dd72d1ea
sayujkuickel <sayujkuickel@gmail.com>
2024-06-22 22:52:37 +0545

    FIX: Header Bug FIx in mobile version

--- src/components/globals/header/ForMobile.jsx
+++ src/components/globals/header/ForMobile.jsx
@@ -19,32 +19,32 @@ const MobileNav = () => {
 
   return (
     <div className="">
-      <div className="z-40 fixed top-0 left-0 right-0 bottom-0 w-screen h-screen overflow-x-hidden overflow-y-scroll">
-        <div className="flex justify-between gap-8 items-center px-[24px] py-2 shadow bg-white w-full">
-          <div className="py-2">
-            <Link href={"/"}>
-              <Image
-                src={`/assets/logo.svg`}
-                width={400}
-                height={400}
-                alt="Main Logo"
-                className="object-contain w-[200px]"
-                priority
-              />
-            </Link>
-          </div>
-          <div className="">
-            <div
-              className="text-[44px]"
-              onClick={() => {
-                setOpenMenu(true);
-              }}
-            >
-              <CgMenuRight />
-            </div>
+      {/* <div className="z-40 fixed top-0 left-0 right-0 bottom-0 w-screen h-screen overflow-x-hidden overflow-y-scroll"> */}
+      <div className="flex justify-between gap-8 items-center px-[24px] py-2 shadow bg-white w-full">
+        <div className="py-2">
+          <Link href={"/"}>
+            <Image
+              src={`/assets/logo.svg`}
+              width={400}
+              height={400}
+              alt="Main Logo"
+              className="object-contain w-[200px]"
+              priority
+            />
+          </Link>
+        </div>
+        <div className="">
+          <div
+            className="text-[44px]"
+            onClick={() => {
+              setOpenMenu(true);
+            }}
+          >
+            <CgMenuRight />
           </div>
         </div>
       </div>
+      {/* </div> */}
       {openMenu && (
         <div className="w-full">
           <div
