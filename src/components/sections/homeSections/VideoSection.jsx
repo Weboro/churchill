0e413dedaf9b8ea8cc5eb8ad8e1c3f117e41b1ba
abhishek <dhitalabhishek982gmail.com>
2025-05-09 11:31:53 +0545

    changes from word file

--- src/components/sections/homeSections/VideoSection.jsx
+++ src/components/sections/homeSections/VideoSection.jsx
@@ -5,21 +5,21 @@ const videoFiles = [
     "cihe-vid-two.mp4",
     "cihne-vid-three.mp4",
     "cihne-vid-four.mp4",
-
 ];
 
 const VideoSection = () => {
     return (
-        <div className="container mx-auto px-5">
+        <div className="container mx-auto px-0 md:px-0 lg:px-20">
             <h2 className="text-3xl font-bold mb-12 text-center">Explore Churchill Institute of Higher Education</h2>
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
                 {videoFiles.map((file, index) => (
                     <div key={index} className="aspect-[9/16] overflow-hidden rounded-lg">
                         <video
-                            src={`/assets/videos/${file}`}
+                            src={`/assets/videos/${file}#t=0.001`}
                             controls
                             muted
                             loop
+                            preload="metadata"
                             className="w-full h-full object-cover"
                         />
                     </div>
