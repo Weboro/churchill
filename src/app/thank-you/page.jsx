49a46877a37232ce2149aa3e1553d490e624633d
abhishek <dhitalabhishek982gmail.com>
2025-05-25 16:40:15 +0545

    tahnk you page content cxhnge

--- src/app/thank-you/page.jsx
+++ src/app/thank-you/page.jsx
@@ -4,7 +4,7 @@ import { Button } from "@/components";
 const page = () => {
   return (
     <div
-      className="flex flex-col gap-[32px] pbon-[10rem]"
+      className="flex flex-col gap-[32px] pbon-[10rem] py-[100px]"
       style={{
         backgroundImage: `linear-gradient(to top, white, #00000000), url('/assets/hero-vector.svg')`,
         backgroundRepeat: "no-repeat",
@@ -20,6 +20,9 @@ const page = () => {
       <h2 className="text-3xl lg:text-5xl font-bold w-fit mx-auto">
         Form submitted successfully!
       </h2>
+      <p className="text-md lg:text-md w-fit mx-auto">
+        Someone will get in touch with you very shortly.
+      </p>
 
       <a href="/" className="w-fit mx-auto block">
         <Button btnName={"Return To Home page"} />
